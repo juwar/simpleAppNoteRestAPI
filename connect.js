34258c11a80dfde799717853c03a85f9cfec935c
juwar <50343114+juwar@users.noreply.github.com>
2019-06-21 16:45:59 +0700

    Two

--- connect.js
+++ connect.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE
+    host : process.env.DB_HOST || 'localhost',
+    user : process.env.DB_USER || 'root',
+    password : process.env.DB_PASSWORD || '',
+    database : process.env.DB_DATABASE || 'appnote'
 })
 
 conn.connect (function(err){
